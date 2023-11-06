94ca9969890675d6a07ab14603b8b45b495b3d72
Roy Gardner <roybowesgardner@gmail.com>
2023-11-06 09:33:00 +0000

    Update ergm_app.py

--- ergm_app.py
+++ ergm_app.py
@@ -91,7 +91,7 @@ st.latex(r'''
 
 st.write('Combines a set of network statistics ${s_1,s_2,…,s_N}$ in proportions determined by set of coefficients ${a_1,a_2,…,a_N}$')
 
-st.write('If the coefficient values are set to zero then:')
+st.write('If all coefficient values are set to zero then:')
 
 st.latex(r'''
     pr(g|G) = \frac{e^0}{\sum\limits_{g=1}^{|G|}e^0)} = \frac{1}{|G|}
